c43059e9b15ef783861b3fb4337084debe8c8bf1
wallis <wallismark@gmail.com>
2023-09-11 11:32:03 +0200

    updates

--- src/main/java/com/example/SpringAcademyTDD/cashcard/CashCardController.java
+++ src/main/java/com/example/SpringAcademyTDD/cashcard/CashCardController.java
@@ -1,21 +1,37 @@
 package com.example.SpringAcademyTDD.cashcard;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cashcard")
 public class CashCardController {
 
+    private CashCardRepository cashCardRepository;
+    CashCardController(CashCardRepository cashCardRepository){
+        this.cashCardRepository = cashCardRepository;
+    }
+
     @GetMapping("/{requestedId}")
-    public ResponseEntity<String> findById(){
-        return ResponseEntity.ok("{}");
+    public ResponseEntity<CashCard> findById(@PathVariable Long id){
+        Optional<CashCard> optionalCashCard = cashCardRepository.findById(id);
+        if (optionalCashCard.isPresent()){
+            return ResponseEntity.ok(optionalCashCard.get());
+        }else
+        {
+            return ResponseEntity.notFound().build();
+        }
+
     }
 }
+
+
+
+
